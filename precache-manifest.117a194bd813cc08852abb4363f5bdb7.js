48b1c4ae16405fba04027e4affe292b24337258a
Uzair Kath <uzairkath@gmail.com>
2020-06-28 18:03:50 +0500

    Updates

---
+++ precache-manifest.117a194bd813cc08852abb4363f5bdb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "394aef2c18bcb8a07daac26a00609a46",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "7c0132c612644510239c",
+    "url": "/expense-tracker/static/css/main.d291d656.chunk.css"
+  },
+  {
+    "revision": "f368cdae4529b7513f97",
+    "url": "/expense-tracker/static/js/2.b942901b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.b942901b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c0132c612644510239c",
+    "url": "/expense-tracker/static/js/main.66f77376.chunk.js"
+  },
+  {
+    "revision": "7b2bb647e90146910206",
+    "url": "/expense-tracker/static/js/runtime-main.5ef6631f.js"
+  }
+]);
